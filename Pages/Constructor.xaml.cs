e80c9e068dc2fe560758bbf13d80b53ac26656b4
Fame21 <toptuk98@gmail.com>
2022-05-09 04:12:56 +0500

    A LOT

--- Pages/Constructor.xaml.cs
+++ Pages/Constructor.xaml.cs
@@ -1,18 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace SpriteGenerator.Pages
 {
@@ -22,88 +13,181 @@ namespace SpriteGenerator.Pages
     public partial class Constructor : Page
     {
         //head
-        int HeadX = 0;
-        int HeadY = 0;
-        int HeadWidth = 23;
-        int HeadHeight = 20;
-        //body
-        int BodyX = 0;
-        int BodyY = 32;
-        int BodyWidth = 23;
-        int BodyHeight = 20;
-        //legs
-        int LegsX = 0;
-        int LegsY = 80;
-        int LegsWidth = 23;
-        int LegsHeight = 20;
+        int headX = 1;
+        int headY = 1;
+        int headWidth = 25;
+        int headHeight = 22;
 
-        List<BitmapImage> colorSheets = new List<BitmapImage>();
-        int currentHeadIdx = 0;
-        int currentBodyIdx = 0;
-        int currentLegsIdx = 0;
-        BitmapImage Original = new BitmapImage(new Uri("../../Images/Idle/Original/Idle.PNG", UriKind.Relative));
-        BitmapImage ColorBlue = new BitmapImage(new Uri("../../Images/Idle/ColorBlue/Idle.PNG", UriKind.Relative));
-        BitmapImage ColorGreen = new BitmapImage(new Uri("../../Images/Idle/ColorGreen/Idle.PNG", UriKind.Relative));
+        //body
+        int bodyX = 1;
+        int bodyY = 1;
+        int bodyWidth = 25;
+        int bodyHeight = 22;
+
+        //legs
+        int legsX = 1;
+        int legsY = 1;
+        int legsWidth = 15;
+        int legsHeight = 17;
+
+        BitmapImage head = new BitmapImage(new Uri("../../Images/redacted/head.png", UriKind.Relative));
+        BitmapImage body = new BitmapImage(new Uri("../../Images/redacted/body.png", UriKind.Relative));
+        BitmapImage legs = new BitmapImage(new Uri("../../Images/redacted/legs.png", UriKind.Relative));
+
+        
+
+
 
         public Constructor()
         {
             InitializeComponent();
-            colorSheets.Add(Original);
-            colorSheets.Add(ColorBlue);
-            colorSheets.Add(ColorGreen);
-            Head.Source = new CroppedBitmap(colorSheets[currentHeadIdx], new Int32Rect(HeadX, HeadY, HeadWidth, HeadHeight));
-            Body.Source = new CroppedBitmap(colorSheets[currentBodyIdx], new Int32Rect(BodyX, BodyY, BodyWidth, BodyHeight));
-            Legs.Source = new CroppedBitmap(colorSheets[currentLegsIdx], new Int32Rect(LegsX, LegsY, LegsWidth, LegsHeight));
+            update();
 
         }
 
+        private WriteableBitmap mergeParts()
+        {
+            WriteableBitmap amalgam = new WriteableBitmap(
+                27, // width
+                49, // height
+                96, // dpiX
+                96, // dpiY
+                PixelFormats.Bgra32, // pixel format
+                null); // bitmap pallete
+
+            BitmapSource headSource = new CroppedBitmap(head, new Int32Rect(headX, headY, headWidth, headHeight));
+            BitmapSource bodySource = new CroppedBitmap(body, new Int32Rect(bodyX, bodyY, bodyWidth, bodyHeight));
+            BitmapSource legsSource = new CroppedBitmap(legs, new Int32Rect(legsX, legsY, legsWidth, legsHeight));
+
+            byte[] headPixels = BitmapSourceToArray(headSource);
+            byte[] bodyPixels = BitmapSourceToArray(bodySource);
+            byte[] legsPixels = BitmapSourceToArray(legsSource);
+
+
+            byte[] amalgamPixels = BitmapSourceToArray(amalgam);
+
+
+            int amalgamPixelsIdx = 0;
+            int PixelsIdx = 0;
+
+            for (int i = 0; i < legsSource.PixelHeight; i++)
+            {
+                for (int j = 0; j < legsSource.PixelWidth; j++)
+                {
+                    amalgamPixelsIdx = j * 4 + (i * 4 * amalgam.PixelWidth) + (amalgam.PixelWidth * 31 * 4 + 6 * 4);
+                    PixelsIdx = i * 4 * legsSource.PixelWidth + j * 4;
+                    if (legsPixels[PixelsIdx + 3] != 255)
+                    {
+                        continue;
+                    }
+                    amalgamPixels[amalgamPixelsIdx + 0] = legsPixels[PixelsIdx + 0];
+                    amalgamPixels[amalgamPixelsIdx + 1] = legsPixels[PixelsIdx + 1];
+                    amalgamPixels[amalgamPixelsIdx + 2] = legsPixels[PixelsIdx + 2];
+                    amalgamPixels[amalgamPixelsIdx + 3] = legsPixels[PixelsIdx + 3];
+                }
+            }
+
+            for (int i = 0; i < headSource.PixelHeight; i++)
+            {
+                for (int j = 0; j < headSource.PixelWidth; j++)
+                {
+                    amalgamPixelsIdx = j * 4 + (i * 4 * amalgam.PixelWidth) + (amalgam.PixelWidth * 1 * 4 + 1 * 4);
+                    PixelsIdx = i * 4 * headSource.PixelWidth + j * 4;
+                    if (headPixels[PixelsIdx + 3] != 255)
+                    {
+                        continue;
+                    }
+                    amalgamPixels[amalgamPixelsIdx + 0] = headPixels[PixelsIdx + 0];
+                    amalgamPixels[amalgamPixelsIdx + 1] = headPixels[PixelsIdx + 1];
+                    amalgamPixels[amalgamPixelsIdx + 2] = headPixels[PixelsIdx + 2];
+                    amalgamPixels[amalgamPixelsIdx + 3] = headPixels[PixelsIdx + 3];
+                }
+            }
+
+            for (int i = 0; i < bodySource.PixelHeight; i++)
+            {
+                for (int j = 0; j < bodySource.PixelWidth; j++)
+                {
+                    amalgamPixelsIdx = j * 4 + (i * 4 * amalgam.PixelWidth) + (amalgam.PixelWidth * 18 * 4 + 1 * 4);
+                    PixelsIdx = i * 4 * bodySource.PixelWidth + j * 4;
+                    if (bodyPixels[PixelsIdx + 3] != 255)
+                    {
+                        continue;
+                    }
+                    amalgamPixels[amalgamPixelsIdx + 0] = bodyPixels[PixelsIdx + 0];
+                    amalgamPixels[amalgamPixelsIdx + 1] = bodyPixels[PixelsIdx + 1];
+                    amalgamPixels[amalgamPixelsIdx + 2] = bodyPixels[PixelsIdx + 2];
+                    amalgamPixels[amalgamPixelsIdx + 3] = bodyPixels[PixelsIdx + 3];
+                }
+            }
+
+            //amalgam.WritePixels(new Int32Rect(6, 31, 15, 17), legsPixels, legsSource.PixelWidth * (legsSource.Format.BitsPerPixel / 8), 0);
+            //amalgam.WritePixels(new Int32Rect(1, 1, 25, 22), headPixels, headSource.PixelWidth * (headSource.Format.BitsPerPixel / 8), 0);
+            //amalgam.WritePixels(new Int32Rect(1, 18, 25, 22), bodyPixels, bodySource.PixelWidth * (bodySource.Format.BitsPerPixel / 8), 0);
+
+            amalgam.WritePixels(new Int32Rect(0, 0, 27, 49), amalgamPixels, amalgam.PixelWidth * (amalgam.Format.BitsPerPixel / 8), 0);
+
+            return amalgam;
+        }
+
+        private byte[] BitmapSourceToArray(BitmapSource bitmapSource)
+        {
+            // Stride = (width) x (bytes per pixel)
+            int stride = (int)bitmapSource.PixelWidth * (bitmapSource.Format.BitsPerPixel / 8);
+            byte[] pixels = new byte[(int)bitmapSource.PixelHeight * stride];
+
+            bitmapSource.CopyPixels(pixels, stride, 0);
+
+            return pixels;
+        }
+        
+
         private void update()
         {
-            Head.Source = new CroppedBitmap(colorSheets[currentHeadIdx], new Int32Rect(HeadX, HeadY, HeadWidth, HeadHeight));
-            Body.Source = new CroppedBitmap(colorSheets[currentBodyIdx], new Int32Rect(BodyX, BodyY, BodyWidth, BodyHeight));
-            Legs.Source = new CroppedBitmap(colorSheets[currentLegsIdx], new Int32Rect(LegsX, LegsY, LegsWidth, LegsHeight));
+            WriteableBitmap amalgam = mergeParts();
+            Char.Source = amalgam;
         }
 
 
         private void NextHead(object sender, RoutedEventArgs e)
         {
-            currentHeadIdx++;
-            currentHeadIdx = currentHeadIdx % colorSheets.Count;
+            headX += headWidth + 1;
+            headX = headX % (head.PixelWidth - 1);
             update();
         }
 
         private void NextBody(object sender, RoutedEventArgs e)
         {
-            currentBodyIdx++;
-            currentBodyIdx = currentBodyIdx % colorSheets.Count;
+            bodyX += bodyWidth + 1;
+            bodyX = bodyX % (body.PixelWidth - 1);
             update();
         }
 
         private void NextLegs(object sender, RoutedEventArgs e)
         {
-            currentLegsIdx++;
-            currentLegsIdx = currentLegsIdx % colorSheets.Count;
+            legsX += legsWidth + 1;
+            legsX = legsX % (legs.PixelWidth - 1);
             update();
         }
 
         private void PrevLegs(object sender, RoutedEventArgs e)
         {
-            currentLegsIdx--;
-            currentLegsIdx = (currentLegsIdx + 3) % colorSheets.Count;
+            legsX -= legsWidth + 1;
+            legsX = (legsX + legs.PixelWidth - 1) % (legs.PixelWidth - 1);
             update();
         }
 
         private void PrevBody(object sender, RoutedEventArgs e)
         {
-            currentBodyIdx--;
-            currentBodyIdx = (currentBodyIdx + 3) % colorSheets.Count;
+            bodyX -= bodyWidth + 1;
+            bodyX = (bodyX + body.PixelWidth - 1) % (body.PixelWidth - 1);
             update();
         }
 
         private void PrevHead(object sender, RoutedEventArgs e)
         {
-            currentHeadIdx--;
-            currentHeadIdx = (currentHeadIdx + 3) % colorSheets.Count;
+            headX -= headWidth + 1;
+            headX = (headX + head.PixelWidth - 1) % (head.PixelWidth - 1);
             update();
         }
     }

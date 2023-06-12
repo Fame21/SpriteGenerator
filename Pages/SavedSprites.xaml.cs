279463694e1d4653a360df8ed94a0b27b249cae0
Fame21 <toptuk98@gmail.com>
2023-06-13 00:30:28 +0500

    release 1.1

--- Pages/SavedSprites.xaml.cs
+++ Pages/SavedSprites.xaml.cs
@@ -15,13 +15,17 @@ namespace SpriteGenerator.Pages
     public partial class SavedSprites : Page
     {
 
+        private BitmapImage bitmap;
         string[] imageFiles = new string[0];
         int pageCount = 1;
         int currentPage = 1;
 
         private void LoadImages()
         {
+            pageCount = 1;
             string dirPath = @"../../SavedSprites";
+            imageFiles = new string[0];
+            bitmap = null;
             if (Directory.Exists(dirPath))
             {
                 imageFiles = Directory.GetFiles(dirPath, "*.png"); // Здесь можно указать нужное расширение файлов
@@ -39,18 +43,19 @@ namespace SpriteGenerator.Pages
             int pageInc = (id - 1) * 15;
             for (int i = 0; i < 15; i++)
             {
-                TextBlock nameControl = ((TextBlock)this.FindName($"Name{i + 1}"));
+                TextBlock nameControl = ((TextBlock)FindName($"Name{i + 1}"));
                 Image imageControl = (Image)FindName($"Pic{i + 1}");
                 try
                 {
-                    BitmapImage bitmap = new BitmapImage();
+                    bitmap = new BitmapImage();
                     bitmap.BeginInit();
+                    bitmap.CacheOption = BitmapCacheOption.OnLoad;
                     bitmap.UriSource = new Uri(Path.GetFullPath(imageFiles[i + pageInc]));
                     bitmap.EndInit();
-                    imageControl.Source = bitmap;
+                    imageControl.Source = bitmap.Clone();
                     nameControl.Text = Path.GetFileNameWithoutExtension(imageFiles[i + pageInc]);
                 }
-                catch (IndexOutOfRangeException)
+                catch (Exception ex)
                 {
                     imageControl.Source = null;
                     nameControl.Text = "Empty";
@@ -111,8 +116,8 @@ namespace SpriteGenerator.Pages
             }
 
             string idx = selected.Name.Substring(6);
-            string name = ((TextBlock)this.FindName("Name" + idx)).Text;
-            Image image = (Image)this.FindName("Pic" + idx);
+            string name = ((TextBlock)FindName("Name" + idx)).Text;
+            Image image = (Image)FindName("Pic" + idx);
 
             if (image.Source == null)
             {
@@ -147,7 +152,53 @@ namespace SpriteGenerator.Pages
         }
         private void DeleteSelectedSprite(object sender, RoutedEventArgs e)
         {
-            //
+            if (selected == null)
+            {
+                MessageBox.Show("Выберите спрайт персонажа", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+                return;
+            }
+
+            string idx = selected.Name.Substring(6);
+            TextBlock name = (TextBlock)FindName("Name" + idx);
+            Image image = (Image)FindName("Pic" + idx);
+
+            if (image.Source == null)
+            {
+                MessageBox.Show("Выбрана пустая ячейка", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+                return;
+            }
+
+            try
+            {
+                var deletingPath = imageFiles[int.Parse(idx)-1];
+                // Check if the file exists before deleting
+                if (File.Exists(deletingPath))
+                {
+                    UnselectSprite();
+                    image.Source = null;
+                    name.Text = "Empty";
+                    bitmap = null;
+
+                    GC.Collect();
+                    GC.WaitForPendingFinalizers();
+
+                    File.Delete(deletingPath);
+                    MessageBox.Show("Спрайт успешно удален", "Успех", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+                else
+                {
+                    MessageBox.Show($"{deletingPath}", "Неудача", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+            finally
+            {
+                LoadImages();
+                LoadPage(currentPage);
+            }
         }
 
     }
